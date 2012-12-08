1b458238c65f8f440f2b269179cc44957ff94f8c
Victor Regalado <vrrs0988@gmail.com>
2012-12-08 17:22:30 -0500

    Update Consts.h

--- Consts.h
+++ Consts.h
@@ -16,7 +16,7 @@ class Consts{
 		const static int MOTOR1_CHANNEL_A=2;        //YELLOW
 		const static int MOTOR1_CHANNEL_B=43;       // WHITE
 		
-		const static int MOTOR2_CHANNEL_A=3;       //YELLOW
+		const static int MOTOR2_CHANNEL_A=3;       //YELLOW  
 		const static int MOTOR2_CHANNEL_B=45;       //WHITE
 		
 		const static float center_dist=0.1145;
@@ -25,11 +25,11 @@ class Consts{
 		//linear model param
 		const static float alpha0u1=0.00000486;
 		const static float alpha1u1=0;
-	    const static float alpha0u2=0.00000486;
+	        const static float alpha0u2=0.00000486;
 		const static float alpha1u2=0;
 		
 		int MOTOR_CHANNELS[4];
-		const static long PERIOD_CYCLE=1000L;
+		const static unsigned long PERIOD_CYCLE=1000L;
 		const static int NPPR=32;
 		
 		const static int NUMBER_PARTITIONS_INTERVAL=1000;

9435bb278221405040c899b81821c0c5dd3682a9
Victor Regalado <vrrs0988@gmail.com>
2012-12-07 23:12:30 -0500

    Update Measurements.h

--- Measurements.h
+++ Measurements.h
@@ -12,7 +12,23 @@ class Measurements{
 		long startime[2];
 		
 		void set_direction(int i,int j,int motor);
-		Consts consts;
+		static Consts consts;
+		
+		static volatile long     		  timer0;
+		static volatile long     		  timer1;
+		static volatile long     		  timer2;
+		static volatile bool             flag0[2];
+		static volatile bool             flag1[2];
+		static volatile bool             flag2[2];
+		static struct pt                 ptsensor0;
+		static struct pt                 ptsensor1;
+		static struct pt                 ptsensor2;
+		
+		static int sensor0thread(struct pt *ptt); 
+		static int sensor1thread(struct pt *ptt); 
+		static int sensor2thread(struct pt *ptt); 
+		
+		bool measuring;
 		
 	public:
 		Measurements();
@@ -25,8 +41,11 @@ class Measurements{
 		
 		static const int CA1_INTERRUPT=0;
 		static const int CA2_INTERRUPT=1;
-		
+		static volatile bool  objflag[2];
+		static volatile float objdist[2];
 		void start_measuring();
 		void channelA(int i,int j);
+		void schedule_sensors();
+		void stop_measuring();
 };
 #endif

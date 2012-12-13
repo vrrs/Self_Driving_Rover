f98304c4e0ebd703ff188a0d32ef6aadeb13e630
Victor Regalado <vrrs0988@gmail.com>
2012-12-13 05:06:43 -0500

    Update examples/tests/test_motion/test_motion.ino

--- examples/tests/test_motion/test_motion.ino
+++ examples/tests/test_motion/test_motion.ino
@@ -3,29 +3,39 @@
 Motion_Controller cntr;
 
 //initial interval before turning
-const unsigned long dt=10000;
+const unsigned long dt=2000;
+unsigned long t0;
+boolean flag;
 
 void setup(){
-
 	attachInterrupt(Measurements::CA1_INTERRUPT,channelA1,CHANGE);
 	attachInterrupt(Measurements::CA2_INTERRUPT,channelA2,CHANGE);
-	
-        //delay before moving
-	delay(2000);
 
       //set up path in polar coordinates
-	cntr.theta_p[0]=M_PI/4;
+	cntr.theta_p[0]=M_PI/2;
 	cntr.dist_p[0]=3;
-	cntr.theta_p[1]=M_PI/8;
-	cntr.dist_p[1]=4;
-        cntr.path_activate=true;
+/*	cntr.theta_p[1]=M_PI/4;
+	cntr.dist_p[1]=2;
+        cntr.theta_p[2]=M_PI/8;
+	cntr.dist_p[2]=4;
+*/
+        cntr.set_initial_speed();
+       
+        //cntr.measurements.start_measuring();
+        t0=millis();
+        flag=true;
 }
 
 
 void loop(){
 	cntr.wheels.schedule_wheel_motion();
+        cntr.measurements.schedule_sensors();
 	cntr.schedule_path();
 	cntr.schedule_speed_control();	
+//if (millis()-t0>dt && flag) {
+  cntr.path_activate=true;
+ // flag=false;
+//}
 }
 
 void channelA1(){
